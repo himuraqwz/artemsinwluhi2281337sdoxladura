db4f7272ccf3cdbe6f336c957bddd9c09520c86b
himuraqwz <himuraqwz@gmail.com>
2024-02-29 20:29:10 +0500

    first version

---
+++ itproger_3/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <clocale>
+#include <string>
+#include <functional>
+#include <vector>
+
+using std::cout;
+using std::string;
+using std::endl;
+using std::function;
+using std::cin;
+
+int main() {
+	setlocale(LC_ALL, "RU");
+
+
+
+	return 0;
+}
